1e34b6e46c3863233ff3569530391f7714741f32
arsh1503 <arshgupta1503@gmail.com>
2026-03-26 16:24:54 +0530

    conflict

--- GVR.js
+++ GVR.js
@@ -19,3 +19,48 @@ function showVideo() {
 window.onload = () => {
     setSuggestions(["Hello"]);
 };
+
+// for Login
+function openPopup() {
+    const popup = document.getElementById("loginPopup");
+    popup.style.display = "flex";
+    setTimeout(() => {
+        popup.classList.add("show");
+    }, 10);
+}
+
+function closePopup() {
+    const popup = document.getElementById("loginPopup");
+    popup.classList.remove("show");
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 300);
+}
+
+window.onclick = function(e) {
+    let popup = document.getElementById("loginPopup");
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+}
+
+function handleLogin() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("login-msg");
+  if (username === "" || password === "") {
+    msg.textContent = "Please fill all fields";
+    return;
+  }
+  msg.textContent = "Login successful 🎉";
+  setTimeout(() => {
+    closePopup();
+    msg.textContent = "";
+  }, 1000);
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "Enter") {
+    handleLogin();
+  }
+});
